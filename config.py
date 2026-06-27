42485f31fc1ba8a23795f6bda86746e0bf18bc45
dalmatinec <temirbekov1602@gmail.com>
2026-06-27 15:31:30 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,23 +1,7 @@
-# config.py
-# ==========================
-# Telegram
-# ==========================
-
 BOT_TOKEN = "8915979890:AAGnbVlfZDVwDyeIrTqfAGYcpTQCyoxhoJQ"
-
 ADMIN_ID = 8243233601
-
 DEVELOPER_USERNAME = "@kz777LLL"
-
-# ==========================
-# Database
-# ==========================
-
 DATABASE_NAME = "database.db"
-
-# ==========================
-# Support Project
-# ==========================
 
 PRICE_1_MONTH = 2500
 PRICE_3_MONTH = 6500
@@ -25,71 +9,19 @@ PRICE_6_MONTH = 10000
 PRICE_12_MONTH = 17500
 PRICE_LIFETIME = 35000
 
-# Telegram Stars (XTR)
-# Заполним после выбора окончательных цен в Stars
-
 STARS_1_MONTH = 215
 STARS_3_MONTH = 560
 STARS_6_MONTH = 855
 STARS_12_MONTH = 1500
 STARS_LIFETIME = 3000
 
-# ==========================
-# Premium
-# ==========================
-
 MONTH_DAYS = 30
 THREE_MONTHS_DAYS = 90
 SIX_MONTHS_DAYS = 180
 YEAR_DAYS = 365
-
 LIFETIME = 99999
 
-# ==========================
-# TikTok
-# ==========================
-
-TIKTOK_PRIMARY = "ytdlp"
-TIKTOK_FALLBACK = "cobalt"
-
-# ==========================
-# Anti Flood
-# ==========================
-
-FLOOD_SECONDS = 3
-
-FLOOD_MESSAGE = "⏳ Пожалуйста, подождите несколько секунд перед следующим запросом."
-
-# ==========================
-# Temp Files
-# ==========================
-
 TEMP_FOLDER = "temp"
-
 DELETE_AFTER_SEND = True
-
-# ==========================
-# Broadcast
-# ==========================
-
-BROADCAST_DELAY = 0.05
-
-# ==========================
-# Download
-# ==========================
-
-MAX_SIMULTANEOUS_DOWNLOADS = 2
-
-DOWNLOAD_TIMEOUT = 300
-
-# ==========================
-# Logging
-# ==========================
-
+FLOOD_SECONDS = 3
 LOG_FILE = "bot.log"
-
-# ==========================
-# Cobalt API
-# ==========================
-
-COBALT_API = "https://co.wuk.sh/api/json"
