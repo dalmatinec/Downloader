f16850f86f44fe30aa87dcb66a20194b8f1149ea
dalmatinec <temirbekov1602@gmail.com>
2026-07-02 23:06:51 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,27 +1,35 @@
-BOT_TOKEN = "8995798622:AAGufN8J5PiZzCqujAILqe-fFbU2wuvhw5Y"
-SUPER_ADMIN_ID = 7982614573
+BOT_TOKEN = "8830635134:AAFQLa0Naw4JMLCeElYIQ_rKc8jHQRF4bKA"
+ADMINS = [7982614573]  # Список администраторов
 
-CHAT_URL = "https://t.me/your_chat_link"
-CHANNEL_URL = "https://t.me/your_channel_link"
+BOT_USERNAME = "@KeshaBot"
+CHANNEL_ID = -1001234567890
+CHAT_ID = -1001234567890
 
-YOUTUBE_URL = ""
-INSTAGRAM_VITALIY = ""
-INSTAGRAM_LYUBASHKA = ""
-TIKTOK_VITALIY = ""
-TIKTOK_LYUBASHKA = ""
+# Основные ссылки
+CHANNEL_LINK = "https://t.me/vashemurlichestvo"
+CHAT_LINK = "https://t.me/vashemurlichestvo"
 
-DONATE_CARD = ""
-DONATE_KASPI = ""
-DONATE_BOOSTY = ""
-DONATE_OTHER = ""
+# Социальные сети
+YOUTUBE_LINK = "https://www.youtube.com/@vashemurlichestvo"
+TIKTOK_LINK = "https://tiktok.com/@kindcatman22"
+INSTAGRAM_LINK = "https://instagram.com/vashemur"
 
-DB_NAME = "bot_database.db"
+# Дополнительные ссылки
+INSTAGRAM_VITALIY = "https://instagram.com/sircatche"
+TIKTOK_LYUBASHKA = "https://tiktok.com/@user1520017034745"
+FACEBOOK_LINK = "https://facebook.com/vitalysirostanov"
+TWITCH_LINK = "https://twitch.tv/catman_kz"
+VK_LINK = "https://vk.com/vashemur"
+PAYPAL_LINK = "https://www.paypal.com/paypalme/vashemur"
 
-AI_API_KEY = ""
-AI_MODEL = "gpt-3.5-turbo"
+# Реквизиты
+KASPI_VISA = "4400 4303 6627 7470 (Виталий С.)"
+BANK_CENTERKREDIT = "4899 9333 7443 8883 (Любовь С.)"
+FREEDOM_BANK = "4002 8900 4964 4242 (Виталий С.)"
+SBER = "4276 4412 0202 5651 (Виталий Юрьевич С.)"
+VTB = "2200 2488 1548 7904 (Виталий Юрьевич С.)"
+TINKOFF = "5536 9140 1530 4763 (Виталий Юрьевич С.)"
+PAYPAL = "https://www.paypal.com/paypalme/vashemur"
 
-MAX_BOOKS_PER_PAGE = 20
-LOG_LIMIT = 100
-BROADCAST_DELAY = 0.05
-
-CHAT_ID = -1004480019939
+# Почта для вопросов
+EMAIL = "antibooznik@gmail.com"
